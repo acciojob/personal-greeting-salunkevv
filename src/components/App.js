e837bd5a0bfe2501e2049a0da7d7ba26cdaccd85
salunkevv <86159929+salunkevv@users.noreply.github.com>
2024-01-15 17:24:13 +0000

    finall

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [val,setVal]=useState("");
+  function func(e)
+  {
+    setVal(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name :</label><br/>
+        <input onChange={func}/>
+        <p>{val}</p>
     </div>
   )
 }
