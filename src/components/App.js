cffc0a321c87c439cbe29d9bf024f84e96a54984
salunkevv <86159929+salunkevv@users.noreply.github.com>
2024-01-15 17:33:20 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,22 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [flag,setFlag]=useState(false)
   let [val,setVal]=useState("");
   function func(e)
   {
+    setFlag(true)
     setVal(e.target.value);
+    
   }
   return (
     <div>
         {/* Do not remove the main div */}
         <label>Enter your name :</label><br/>
         <input onChange={func}/>
-        <p>{val}</p>
+        {flag &&
+        <p>Hello {val}!</p>
+}
     </div>
   )
 }
